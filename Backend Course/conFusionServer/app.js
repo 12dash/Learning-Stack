0a556433f43061b8c0bba5d1e8650e3f19b2a013
12dash <soham.dandapath@gmail.com>
2020-10-01 15:58:24 +0800

    Cookies

--- Backend Course/conFusionServer/app.js
+++ Backend Course/conFusionServer/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
@@ -32,38 +33,55 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
-function auth(req,res, next) {
-  console.log(req.headers);
+function auth(req, res, next) {
+  console.log(req.signedCookies);
 
-  var authHeader = req.headers.authorization;
-  if(!authHeader){
-    var err = new Error('You are not authenticated!');
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
 
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    return next(err);
-  }
+    if (!authHeader) {
+      var err = new Error('You are not authenticated!');
 
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var username = auth[0];
-  var password = auth[1];
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
 
-  if (username == 'admin' &&password == 'password'){
-    next();
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
+
+    if (username == 'admin' && password == 'password') {
+      res.cookie('user', 'admin',{signed: true});
+      next();
+    }
+    else {
+      var err = new Error('Wrong credentials... You are not authenticated!');
+
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
   else{
-    var err = new Error('Wrong credentials... You are not authenticated!');
+    if(req.signedCookies.user === 'admin'){
+      next();
+    }
+    else{
+      var err = new Error('Wrong cookies... You are not authenticated!');
 
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    return next(err);
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
+
 
 }
 
-app.use(auth); 
+app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
